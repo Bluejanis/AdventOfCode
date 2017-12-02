6e446af721f726ec89bf618d3a71c497cf84895b
Janis P <jpuetz@uos.de>
2017-12-02 00:04:37 -0500

    task2

---
+++ Day1/src/task2.kt
@@ -0,0 +1,23 @@
+import java.lang.Integer.parseInt
+
+fun main(args: Array<String>) {
+    if (args.size != 1)
+    {
+        println("Wrong number of parameters, expected exactly 1, but got " +  args.size)
+        return
+    }
+
+    val string = args[0]
+    var result = 0
+    val stepSize = string.length / 2
+    for (index in 0 until string.length) {
+        val current : Int = parseInt(string[index].toString())
+        val compareToIndex = (index + stepSize) % string.length
+        val compareTo : Int = parseInt(string[compareToIndex].toString())
+        if (current == compareTo)
+        {
+            result += current
+        }
+    }
+    println("Result: " + result)
+}

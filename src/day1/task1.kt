6ee98411ccd5cdc0d04205a77f4d4be45ca8db61
Janis P <jpuetz@uos.de>
2017-12-03 02:34:09 -0500

    moved day1 to inner folder/package

--- src/day1/task1.kt
+++ src/day1/task1.kt
@@ -1,3 +1,5 @@
+package day1
+
 import java.lang.Integer.parseInt
 
 fun main(args: Array<String>) {
@@ -9,10 +11,9 @@ fun main(args: Array<String>) {
 
     val string = args[0]
     var result = 0
-    val stepSize = string.length / 2
     for (index in 0 until string.length) {
         val current : Int = parseInt(string[index].toString())
-        val compareToIndex = (index + stepSize) % string.length
+        val compareToIndex = (index + 1) % string.length
         val compareTo : Int = parseInt(string[compareToIndex].toString())
         if (current == compareTo)
         {
--- src/day1/task2.kt
+++ src/day1/task2.kt
@@ -1,3 +1,5 @@
+package day1
+
 import java.lang.Integer.parseInt
 
 fun main(args: Array<String>) {
@@ -9,9 +11,10 @@ fun main(args: Array<String>) {
 
     val string = args[0]
     var result = 0
+    val stepSize = string.length / 2
     for (index in 0 until string.length) {
         val current : Int = parseInt(string[index].toString())
-        val compareToIndex = (index + 1) % string.length
+        val compareToIndex = (index + stepSize) % string.length
         val compareTo : Int = parseInt(string[compareToIndex].toString())
         if (current == compareTo)
         {
